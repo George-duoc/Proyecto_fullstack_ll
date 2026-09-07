95291a3d4f87324b503e16372a35f8cf2d945fef
George Giovine <ge.giovine@duocuc.cl>
2026-09-06 18:49:41 -0700

    Carrito.js html y modificaciones de autos.html + catalogo.js

---
+++ carrito.js
@@ -0,0 +1,48 @@
+const contenedorCarrito = document.getElementById('contenedor-carrito');
+const totalCarrito = document.getElementById('total-carrito');
+
+let carrito = JSON.parse(localStorage.getItem('miCarrito')) || [];
+
+
+function renderizarCarrito() {
+    contenedorCarrito.innerHTML = '';
+    let totalPrecio = 0;
+
+    if (carrito.length === 0) {
+        contenedorCarrito.innerHTML = '<div class="alert alert-secondary text-center">Tu carrito está vacío ): ¡Ve al catálogo a seleccionar una reliquia!</div>';
+        totalCarrito.textContent = '0';
+        return;
+    }
+
+
+    carrito.forEach((auto, indice) => {
+        
+        totalPrecio += auto.precio;
+
+        const itemHtml = `
+            <div class="list-group-item d-flex justify-content-between align-items-center shadow-sm mb-2 rounded border-0">
+                <div>
+                    <h6 class="mb-0 fw-bold">${auto.nombre}</h6>
+                    <small class="text-success fw-bold">$${auto.precio.toLocaleString('es-CL')}</small>
+                </div>
+                <button class="btn btn-outline-danger btn-sm" onclick="eliminarDelCarrito(${indice})">
+                    <i class="bi bi-trash"></i>
+                </button>
+            </div>
+        `;
+        
+        contenedorCarrito.innerHTML += itemHtml;
+    });
+
+    totalCarrito.textContent = totalPrecio.toLocaleString('es-CL');
+}
+
+function eliminarDelCarrito(indice) {
+    carrito.splice(indice, 1);
+    
+    localStorage.setItem('miCarrito', JSON.stringify(carrito));
+    
+    renderizarCarrito();
+}
+
+renderizarCarrito();
---
+++ catalogo.js
@@ -0,0 +1,33 @@
+
+let carrito = JSON.parse(localStorage.getItem('miCarrito')) || [];
+
+const botonesComprar = document.querySelectorAll('.btn-comprar');
+const contadorCarrito = document.getElementById('contador-carrito');
+
+
+if (contadorCarrito) {
+    contadorCarrito.textContent = carrito.length;
+}
+
+botonesComprar.forEach(boton => {
+    
+    boton.addEventListener('click', (evento) => {
+        
+        const botonPresionado = evento.target;
+        
+        const auto = {
+            id: botonPresionado.dataset.id,
+            nombre: botonPresionado.dataset.nombre,
+            precio: parseInt(botonPresionado.dataset.precio)
+        };
+
+
+        carrito.push(auto);
+
+        contadorCarrito.textContent = carrito.length;
+        
+        console.log("Carrito actual:", carrito);
+
+        localStorage.setItem('miCarrito', JSON.stringify(carrito));
+    });
+});
